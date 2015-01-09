5316a0ba2f0bc96ebe7dbf04be7c062632d62146
Kiwiheretic (aka Splat) <kiwiheretic@myself.com>
2015-01-09 14:50:23 +1300

    Make logos_router.py DRY

--- logos/logos_router.py
+++ logos/logos_router.py
@@ -1,44 +1,38 @@
 # LogosRouter.py
-import pdb
 
-bible_tables = ('BibleTranslations', 'BibleBooks', 
-               'BibleVerses', 'BibleConcordance', 
-               'BibleDict')
-
-settings_tables = ('Settings',
-                 'BibleColours', 'RoomOptions',)
-
-
-scripture_game_tables = ('GameGames', 'GameUsers', 'GameSolveAttempts',
-                         'Scriptures')
-
+# Follows is a list of DB identifier and associated model names
+routing_data = (('bibles', 
+                 ('BibleTranslations', 'BibleBooks', 
+                  'BibleVerses', 'BibleConcordance', 
+                  'BibleDict')),
+                ('settings',
+                 ('Settings',
+                  'BibleColours', 'RoomOptions',)),
+                ('game-data',
+                 ('GameGames', 'GameUsers', 'GameSolveAttempts',
+                  'Scriptures'),),
+                  )
+                 
+            
 class LogosRouter(object):
     def db_for_read(self, model, **hints):
         """
         Select DB to read from
         """
-        if model._meta.object_name in bible_tables:
-            return 'bibles'
-        elif model._meta.object_name in settings_tables:
-            return 'settings'
-        elif model._meta.object_name in scripture_game_tables:
-            return 'game-data'        
-        else:
-            return None
-        
+        for db_id, models in routing_data:
+            if model._meta.object_name in models:
+                return db_id
+        return None
+    
+       
     def db_for_write(self, model, **hints):
         """
         Select DB to write to
         """
-        if model._meta.object_name in bible_tables:
-            return 'bibles'
-        elif model._meta.object_name in settings_tables:
-            return 'settings'
-        elif model._meta.object_name in scripture_game_tables:
-            return 'game-data'
-        else:
-            return None        
-
+        for db_id, models in routing_data:
+            if model._meta.object_name in models:
+                return db_id
+        return None        
 
     def allow_relation(self, obj1, obj2, **hints):
         """
@@ -51,54 +45,19 @@ class LogosRouter(object):
         Whether to allow data migrations on this model
         manage.py loaddata seems to not work without this
         """
-        if model._meta.object_name in bible_tables:
-            if db == 'bibles':
-                return True
-            else:
-                return False
-            
-        elif model._meta.object_name in settings_tables:
-            if db == 'settings':
-                return True
-            else:
-                return False                                         
 
-        elif model._meta.object_name in scripture_game_tables:
-            if db == 'game-data':
-                return True
-            else:
-                return False  
-                                         
-        elif db == 'default':
+        for db_id, models in routing_data:
+
+            if model._meta.object_name in models:
+                if db == db_id:
+                    return True
+                else:
+                    return False
+        if db == 'default':
             return True
         else:
-            return False
-    
-    def allow_syncdb(self, db, model):
-        """
-        Sync the tables to appropriate databases,
-        """
-#        print "syncdb",  db, model._meta.object_name
-        if model._meta.object_name in bible_tables:
-            if db == 'bibles':
-                return True
-            else:
-                return False
-            
-        elif model._meta.object_name in settings_tables:
-            if db == 'settings':
-                return True
-            else:
-                return False                                         
+            return False        
+        
+   
 
-        elif model._meta.object_name in scripture_game_tables:
-            if db == 'game-data':
-                return True
-            else:
-                return False  
-                                         
-        elif db == 'default':
-            return True
-        else:
-            return False
         
