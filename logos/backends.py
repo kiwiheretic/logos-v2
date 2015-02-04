e30bfd31666a18ad058edcfd5792508a09504678
Kiwiheretic (aka Splat) <kiwiheretic@myself.com>
2015-02-04 14:33:57 +1300

    Make web login case insensitive (seeing IRC nicks are case insensitive).

---
+++ logos/backends.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.backends import ModelBackend
+#from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+ 
+class CaseInsensitiveModelBackend(ModelBackend):
+    """
+    By default ModelBackend does case _sensitive_ username authentication, which isn't what is
+    generally expected.  This backend supports case insensitive username authentication.
+    """
+    def authenticate(self, username=None, password=None):
+        UserModel = get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+        try:
+            user = UserModel.objects.get(username__iexact=username)
+            if user.check_password(password):
+                return user
+        except UserModel.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a non-existing user (#20760).
+            UserModel().set_password(password)  
--- logos/settings.py
+++ logos/settings.py
@@ -181,7 +181,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend', # this is default
+    'logos.backends.CaseInsensitiveModelBackend', # this is default
     'guardian.backends.ObjectPermissionBackend',
 )
 
