36f708fa930dc7350659b8f00d92d97520c37106
Kiwiheretic (aka Splat) <kiwiheretic@myself.com>
2015-02-17 20:36:05 +1300

    Convert url_catcher plugin over to use database

---
+++ plugins/url_catcher/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class CapturedUrls(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    nick = models.CharField(max_length=90)
+    room = models.CharField(max_length=60)
+    url = models.CharField(max_length=200)
+    class Meta:
+        app_label = 'logos'
--- plugins/url_catcher/plugin.py
+++ plugins/url_catcher/plugin.py
@@ -1,8 +1,10 @@
 # test plugin
 from bot.pluginDespatch import Plugin
 import re
-import pdb
+import datetime
 import logging
+from models import CapturedUrls
+
 from logos.settings import LOGGING
 logger = logging.getLogger(__name__)
 logging.config.dictConfig(LOGGING)
@@ -14,28 +16,35 @@ class UrlCatcherPlugin(Plugin):
         self.urls = []
         
         self.commands = (\
-         (r'urls\s+(\d+)|urls', self.urls_display, "display a list of captured urls"),
+         (r'urls\s+(?P<room>#[a-zA-z0-9-]+)\s+(?P<count>\d+)', self.urls_display, "display a list of captured urls"),
+         (r'urls\s+(?P<room>#[a-zA-z0-9-]+)$', self.urls_display, "display a list of captured urls"),
         )
     
     def urls_display(self, regex, chan, nick, **kwargs):
-        if regex.lastindex == 1:
-            num_to_print = int(regex.group(1))
+        if 'count' in regex.groupdict():
+            num_to_print = int(regex.group('count'))
         else:
             # default to 5 if no number specified
             num_to_print = 5
         # send the urls in order of reverse chronological
         # order
-        for url in reversed(self.urls[:num_to_print]):
-            self.notice(nick, url)
+        cap_urls = CapturedUrls.objects.filter(room=chan.lower()).\
+            order_by('-timestamp')[:num_to_print]
+        for url in cap_urls:
+            timestamp = str(url.timestamp)
+            self.notice(nick, "{} {} -- {}".format(timestamp, url.nick, url.url))
 
     def privmsg(self, user, channel, message):
         # Capture any matching urls and keep it in buffer
-        url_mch = re.match('(?:https?://\S+)|www\.\S+', message, re.I)
+        url_mch = re.search('(?:https?://\S+)|www\.\S+', message, re.I)
         if url_mch:
-            logger.info("capturing url : " + url_mch.group(0))
-            self.urls.append(url_mch.group(0))
-            # keep only the last 20 items in the list
-            self.urls = self.urls[-20:]
+            url = url_mch.group(0)
+            logger.info("capturing url : " + url)
+            timestamp = datetime.datetime.utcnow()
+            cap_url = CapturedUrls(timestamp=timestamp, nick=user, 
+                                   room=channel.lower(), 
+                                   url=url) 
+            cap_url.save()
 
 
             
