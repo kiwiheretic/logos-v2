f00a6e023d90c60993a4b9e50c196d08cde7d568
Kiwiheretic (aka Splat) <kiwiheretic@myself.com>
2015-03-25 13:26:30 +1300

    Automatically add hostname to ALLOWED_HOSTS

--- logos/settings.py
+++ logos/settings.py
@@ -1,6 +1,7 @@
 # Django settings for logos project.
 import os
 import sys
+import socket
 import logging
 import email_settings
 logger = logging.getLogger(__name__)
@@ -126,6 +127,7 @@ BIBLES_DIR = os.path.join(BASE_DIR, 'bibles')
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS.append(socket.gethostname())
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
