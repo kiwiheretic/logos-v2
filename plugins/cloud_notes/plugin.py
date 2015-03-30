70228d5cb76583079d94c2eb199e19b01110cd6b
Kiwiheretic (aka Splat) <kiwiheretic@myself.com>
2015-03-30 13:37:31 +1300

    Fix memo paths and add more features to notes

--- plugins/cloud_notes/plugin.py
+++ plugins/cloud_notes/plugin.py
@@ -13,6 +13,7 @@ logging.config.dictConfig(LOGGING)
 from cloud_notes.models import Note, Folder
 from bot.logos_decorators import login_required
 
+READ_SIZE = 250
 # If using this file as a starting point for your plugin
 # then remember to change the class name 'MyBotPlugin' to
 # something more meaningful.
@@ -28,12 +29,29 @@ class NotesPlugin(Plugin):
         self.commands = ((r'list$', self.list_notes, 'list all notes'),
                          (r'list (?P<range>\d*-\d*)', self.list_notes, 'list all notes'),
                          (r'read (?P<note_id>\d+)', self.read, 'read a note'),
+                         (r'read more', self.read_more, 'more reading of note'),
                          (r'notes on$', self.start_logging, 'start logging bible notes'),
                          (r'notes off$', self.stop_logging, 'start logging bible notes'),
                          )
         self.user_notes = {}
 
+    def _in_usernotes(self, username, key):
+        if username in self.user_notes:
+            if key in self.user_notes[username]:
+                return True
+            else:
+                return False
+        else:
+            return False
+        
+    def _update_usernotes_hash(self, username, dict_values):
+        if username not in self.user_notes:
+            self.user_notes[username] = dict_values
+        else:
+            for k, v in self.user_notes[username].iteritems():
+                self.user_notes[username][k] = v
     def privmsg(self, user, channel, message):
+        
         nick,_ = user.split('!')
         username = self.get_auth().get_username(nick)
         if username and username in self.user_notes:
@@ -41,7 +59,7 @@ class NotesPlugin(Plugin):
             if regex :
                 text = regex.group(1)
                 logger.debug("note_users = " + str(self.user_notes))
-                note = self.user_notes[username]
+                note = self.user_notes[username]['note']
                 note.note += "\n" + text + "\n"
                 dt = datetime.utcnow()
                 utc_tz = pytz.timezone("UTC")                
@@ -54,7 +72,7 @@ class NotesPlugin(Plugin):
         chan = data['chan']
         username = self.get_auth().get_username(nick)
         if username and username in self.user_notes:
-            note = self.user_notes[username]
+            note = self.user_notes[username]['note']
             for verse in data['verses']:
                 verse_txt = " ".join(verse) + "\n"
                 note.note += verse_txt
@@ -69,7 +87,7 @@ class NotesPlugin(Plugin):
         chan = data['chan']
         username = self.get_auth().get_username(nick)
         if username and username in self.user_notes:
-            note = self.user_notes[username]
+            note = self.user_notes[username]['note']
             note.note += "\n"
             for verse_txt in data['verses']:
                 note.note += verse_txt + "\n"
@@ -81,6 +99,7 @@ class NotesPlugin(Plugin):
 
     def onSignal_logout(self, source, data):
         username = data['username']
+        logger.debug("cloud notes: onSignal_logout " + username)
         if username in self.user_notes:
             del self.user_notes[username]
                 
@@ -107,38 +126,46 @@ class NotesPlugin(Plugin):
                         note_type = "bible note",
                         note = "Bible Note: \n")
             note.save()
-        self.user_notes[username] = note
+        self._update_usernotes_hash(username, {'note':note})
         self.say(chan, "Note logging turned on for "+nick)
 
     @login_required()
     def stop_logging(self, regex, chan, nick, **kwargs):
         username = self.get_auth().get_username(nick)
         if username in self.user_notes:
-            del self.user_notes[username]
+            del self.user_notes[username]['note']
         self.say(chan, "Note logging turned off for "+nick)    
-        
+
+
+    
     @login_required()
     def list_notes(self, regex, chan, nick, **kwargs):
-#        import pdb; pdb.set_trace()
         num_to_list = 4
+        username = self.get_auth().get_username(nick)
+        notes = None
         if 'range' in regex.groupdict():
             range = regex.group('range')
             mch1 = re.match(r'-(\d+)$', range) 
             mch2 = re.match(r'(\d+)-$', range)
             mch3 = re.match(r'(\d+)-(\d+)$', range) 
+            mch4 = re.match(r'-$', range) 
             if mch1:
                 lidx = int(mch1.group(1))
                 notes = Note.objects.\
                     filter(user__username = nick.lower(), pk__lte = lidx).\
                     exclude(folder__name = 'Trash').\
                     order_by('-id')[:num_to_list]
+                fidx = notes[num_to_list-1].id
+                self._update_usernotes_hash(username, {'list_index':fidx-1})
             elif mch2:
                 fidx = int(mch2.group(1))
                 notes = Note.objects.\
                     filter(user__username = nick.lower(), pk__gte = fidx).\
                     exclude(folder__name = 'Trash').\
                     order_by('id')[:num_to_list]
+                idx = notes[0].id
                 notes = reversed(notes)
+                self._update_usernotes_hash(username, {'list_index':idx-1})
             elif mch3:
                 fidx = int(mch3.group(1))
                 lidx = int(mch3.group(2))
@@ -147,18 +174,60 @@ class NotesPlugin(Plugin):
                            pk__lte = lidx).\
                     exclude(folder__name = 'Trash').\
                     order_by('-id')[:num_to_list]                
+                self._update_usernotes_hash(username, {'list_index':fidx-1})
+            elif mch4:
+                if self._in_usernotes(username, 'list_index'):
+                    idx = self.user_notes[username]['list_index']
+                    notes = Note.objects.\
+                        filter(user__username = nick.lower(), pk__lte = idx).\
+                        exclude(folder__name = 'Trash').\
+                        order_by('-id')[:num_to_list]
+                    if notes: #if any found
+                        idx = notes[len(notes)-1].id
+                        self.user_notes[username]['list_index'] = idx-1
+                    else:
+                        self.say(chan, "**No more notes found***")
         else:
             notes = Note.objects.filter(user__username = nick.lower()).\
                 exclude(folder__name = 'Trash').order_by('-id')[:num_to_list]
-        for note in notes:
-            self.say(chan, str(note.id) + " " + note.title)
+            lidx = notes[num_to_list-1].id
+            self._update_usernotes_hash(username, {'list_index':lidx-1})
+                            
+        if notes:
+            for note in notes:
+                self.say(chan, str(note.id) + " " + note.title)
 
+    @login_required()
+    def read_more(self, regex, chan, nick, **kwargs):
+        username = self.get_auth().get_username(nick)
+        if username not in self.user_notes:
+            self.say(chan, "Nothing to read")
+        else:
+            if 'reading' in self.user_notes[username]:
+                reading = self.user_notes[username]['reading'].note
+                ridx = self.user_notes[username]['ridx']
+                try:
+                    ridx2 = reading.rindex(" ", ridx, ridx+READ_SIZE)                
+                    notestr = reading[ridx:ridx2].strip()
+                    if notestr != "":
+                        self.say(chan, notestr)
+                    else:
+                        self.say(chan, "**Note End**")
+
+                except ValueError:
+                    self.say(chan, "**Note End**")
+
+    @login_required()
     def read(self, regex, chan, nick, **kwargs):
         note_id = regex.group('note_id')
+        username = self.get_auth().get_username(nick)
         try:
             note = Note.objects.get(pk=note_id)
-            notestr = re.sub(r'\n', ' ', note.note[:800])
-            self.say(chan, notestr)
+            notestr = re.sub(r'\n', ' ', note.note)
+            ridx = notestr.rindex(" ", 0, READ_SIZE)
+            self._update_usernotes_hash(self, username, {'reading':note, 'ridx':ridx})
+
+            self.say(chan, notestr[0:ridx])
         except Note.DoesNotExist:
             self.notice(nick, "Note does not exist")
         
