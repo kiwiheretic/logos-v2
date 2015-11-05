8f7640fd5a145dba724974ca5a46f73b9c991c45
kiwiheretic <kiwiheretic@myself.com>
2015-11-06 09:28:45 +1300

    Fix blockquote missing from markdown filter

--- cloud_notes/templatetags/markdown_filters.py
+++ cloud_notes/templatetags/markdown_filters.py
@@ -1,11 +1,13 @@
 from django import template
 import markdown as md
 import bleach
+import copy
 
 register = template.Library()
 
 def markdown(value):
     """convert to markdown"""
-    return md.markdown(bleach.clean(value))
+    allowed_tags = bleach.ALLOWED_TAGS + ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+    return bleach.clean(md.markdown(value), tags = allowed_tags)
     
 register.filter('markdown', markdown)
