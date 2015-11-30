0818e422c64ea1eae2a9c57b2c9f89ad758cdfd3
kiwiheretic <kiwiheretic@myself.com>
2015-11-30 20:15:30 +1300

    moved cloud memos bot plugin to within cloud memos app

--- cloud_memos/tests.py
+++ cloud_memos/tests.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate
 from django.test import Client
 from django.core.urlresolvers import reverse
 
+from bot.testing.utils import LogosTestCase
+from .bot_plugin import MemosPlugin
+
 # Create your tests here.
 from .models import Memo, Folder
 
@@ -104,3 +107,57 @@ class MemoTestCase(TestCase):
     def tearDown(self):
         self.u1.delete()
         self.u2.delete()
+
+class TestMemos(LogosTestCase):
+    # set plugin_class to the actual class
+    # of the plugin you wish to test
+    plugin_class = MemosPlugin
+ 
+    
+    def setUp(self):
+        # create test users
+        #  https://docs.djangoproject.com/en/1.8/topics/auth/default/#creating-users
+        self.u1 = fred = User.objects.create_user("fred", "fred@nowhere.com", "pass123")
+        self.u2 = john = User.objects.create_user("john", "john@nowhere.com", "pass456")
+        self.u3 = mary = User.objects.create_user("mary", "mary@nowhere.com", "Pass789")
+ 
+ 
+    def testMemoSend(self):
+        
+        self.set_nick("fred")
+        self.login('pass123')
+        
+        output = self.plugin.send_command("send john Hi, How are you")
+        self.assertIn('Memo sent', output)
+
+        self.set_nick("john")
+        self.login("pass456")
+        output = self.plugin.send_command("check")
+        self.assertIn('1 unread', output)
+        
+        output = self.plugin.send_command("read 0")
+        self.assertIn('How are you', output)
+        
+        output = self.plugin.send_command("check")
+        self.assertIn('no unread', output)
+        
+        output = self.plugin.send_command("delete 0")
+        self.assertIn('deleted', output)
+        
+        output = self.plugin.send_command("list")
+        self.assertIn('No memos found', output)
+
+        output = self.plugin.send_command("send Mary Hi, How are you")
+        self.assertIn('Memo sent', output)
+
+        self.set_nick("Mary")
+        self.login("Pass789")
+        output = self.plugin.send_command("check")
+        self.assertIn("You have 1 unread memos", output)
+        
+    def tearDown(self):
+        print ("deleting test users.")
+        self.u1.delete()
+        self.u2.delete()
+        self.u3.delete()
+        
