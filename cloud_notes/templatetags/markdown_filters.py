233e5b2f48ae567f50843dc3b8b4301a21c12b71
kiwiheretic <kiwiheretic@myself.com>
2015-12-29 15:58:29 -0700

    Add pre tag to cloud notes

--- cloud_notes/templatetags/markdown_filters.py
+++ cloud_notes/templatetags/markdown_filters.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 def markdown(value):
     """convert to markdown"""
-    allowed_tags = bleach.ALLOWED_TAGS + ['p', 'br', 'hr', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+    allowed_tags = bleach.ALLOWED_TAGS + ['blockquote', 'pre', 'p', 'br', 'hr', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
     return bleach.clean(md.markdown(value), tags = allowed_tags)
     
 register.filter('markdown', markdown)
