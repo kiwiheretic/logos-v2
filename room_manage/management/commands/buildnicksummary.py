579608a3c2a19f88beceab2088b68e502f68ed0a
kiwiheretic <kiwiheretic@myself.com>
2016-05-24 23:16:23 -0400

    Optimise nick summary table build

--- room_manage/management/commands/buildnicksummary.py
+++ room_manage/management/commands/buildnicksummary.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from room_manage.models import NickHistory, NickSummary
+from django.db import transaction
 
 class Command(BaseCommand):
     help = 'Builds the NickSummary table from NickHistory'
@@ -14,25 +15,37 @@ class Command(BaseCommand):
             pass
             self.stdout.write('Nothing done')
         else:
-            for rec in NickHistory.objects.all():
-                network = rec.network
-                nick = rec.nick
-                host_mask = rec.host_mask
-                time_seen = rec.time_seen
-                try:
-                    obj = NickSummary.objects.get(
-                        network = network,
-                        nick = nick,
-                        host_mask = host_mask)
-                    
-                    if time_seen > obj.last_seen:
-                        obj.last_seen = time_seen
-                        obj.save()
-                except NickSummary.DoesNotExist:
-                    obj = NickSummary(network = network,
-                            nick = nick,
-                            host_mask = host_mask,
-                            last_seen = time_seen)
-                    obj.save()
+            self.create_summary()
+            self.stdout.write('Successfully finished adding data to NickSummary table.')
 
-            self.stdout.write('Successfully finished addung data to NickSummary table.')
+    @transaction.atomic
+    def create_summary(self):
+        last_rec = NickSummary.objects.order_by('last_seen').last()
+        if last_rec:
+            last_rec_dt = last_rec.last_seen
+            self.stdout.write(str(last_rec_dt))
+            qs = NickHistory.objects.filter(time_seen__gte = last_rec_dt).order_by('time_seen')
+        else:
+            qs = NickHistory.objects.all().order_by('time_seen')
+
+        for rec in qs:
+            network = rec.network
+            nick = rec.nick
+            host_mask = rec.host_mask
+            time_seen = rec.time_seen
+            try:
+                obj = NickSummary.objects.get(
+                    network = network,
+                    nick = nick,
+                    host_mask = host_mask)
+                
+                if time_seen > obj.last_seen:
+                    obj.last_seen = time_seen
+                    obj.save()
+            except NickSummary.DoesNotExist:
+                obj = NickSummary(network = network,
+                        nick = nick,
+                        host_mask = host_mask,
+                        last_seen = time_seen)
+                obj.save()
+
