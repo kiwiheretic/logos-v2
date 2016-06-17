6c3fd40df53b2fe1425a790049876d1ccd767f04
kiwiheretic <kiwiheretic@myself.com>
2016-06-16 23:26:33 -0400

    Reworked symbolics server with better flow control

--- symbolics/bot_plugin.py
+++ symbolics/bot_plugin.py
@@ -1,6 +1,7 @@
 # test plugin
 from bot.pluginDespatch import Plugin
 from twisted.internet.protocol import DatagramProtocol
+from twisted.internet.error import AlreadyCalled, AlreadyCancelled
 import re
 import datetime
 import logging
@@ -8,6 +9,7 @@ from ast1 import MyNodeVisitor, SymVars
 import sympy
 import ast
 
+from logos.roomlib import get_global_option, set_global_option
 from django.conf import settings
 logger = logging.getLogger(__name__)
 logging.config.dictConfig(settings.LOGGING)
@@ -15,17 +17,47 @@ logging.config.dictConfig(settings.LOGGING)
 class SymbolicsProtocol(DatagramProtocol):
     def __init__(self, conn):
         self.conn = conn
+        self.connected = False
 
-    def startProtocol(self):
-        self.transport.connect('127.0.0.1', 5001)
-        #self.sendDatagram()
+    def connect(self, chan):
+        self.chan = chan
+        symhost = get_global_option('symbolics-host')
+        host, port = symhost.split(':')
+        if hasattr(self, "t"): 
+            self.t.stopListening()
+        self.t = self.conn.reactor.listenUDP(0, self)
+        self.transport.connect(host, int(port))
+        self.sendDatagram("hello")
+        self.pending = self.conn.reactor.callLater(2.0, self.timeout)
+        self.cancel = False
     
+    def timeout(self):
+        if self.cancel:
+            print "timeout cancelled"
+        else:
+            print "timeout"
+            self.connected = False
+            self.conn.disconnected(self.chan)
+
     def sendDatagram(self, dgram):
         self.transport.write(dgram)
+        self.pending = self.conn.reactor.callLater(2.0, self.timeout)
+        self.cancel = False
 
     def datagramReceived(self, datagram, host):
         print 'Datagram received: ', repr(datagram)
-        self.conn.response(datagram)
+        self.cancel = True
+#        try:
+#            self.pending.cancel()
+#        except (AlreadyCalled, AlreadyCancelled) as e:
+#            print e
+#            pass
+#        else:
+        if datagram == "hello":
+            self.connected = True
+            self.conn.connected(self.chan)
+        else:
+            self.conn.response(datagram)
 
 class SympyPlugin(Plugin):
     plugin = ("sym", "Symbolics")
@@ -34,22 +66,47 @@ class SympyPlugin(Plugin):
         
         self.commands = (\
          (r'connect$', self.connect, "Connect to symbolics server"),
+         (r'set host (?P<host>\S+)$', self.set_host, "Set symbolics host"),
+         (r'show host$', self.show_host, "Set symbolics host"),
          (r'calc (?P<expr>\S.*)', self.calc, "Performs symbolic computation"),
         )
-        self.connected = True 
+
+        symhost = get_global_option('symbolics-host')
+        if not symhost:
+            set_global_option('symbolics-host', '127.0.0.1:5001')
 
         self.protocol = SymbolicsProtocol(self)
-        self.t = self.reactor.listenUDP(0, self.protocol)
         self.q = []
 
+    def set_host(self, regex, chan, nick, **kwargs):
+        symhost = regex.group('host')
+        if re.match(r"\d+", symhost):
+            set_global_option('symbolics-host', '127.0.0.1:'+symhost)
+            self.say(chan, "Symbolics host set to "+ symhost)
+        elif re.match(r"(\d+\.){3}\d+:\d+", symhost):
+            set_global_option('symbolics-host', symhost)
+            self.say(chan, "Symbolics host set to "+ symhost)
+        else:
+            self.say(chan, "Symbolics host, invalid format")
+
+    def show_host(self, regex, chan, nick, **kwargs):
+        symhost = get_global_option('symbolics-host')
+        self.say(chan, "Symbolics host "+ symhost)
+
     def connect(self, regex, chan, nick, **kwargs):
-        self.protocol.startProtocol()
-        self.connected = True
-    
+        self.protocol.connect(chan)
+
+    def connected(self, chan):
+        self.say(chan, "Connected to Symbolics Server")
+
+    def disconnected(self, chan):
+        self.say(chan, "Disconnected from symbolics server")
+
     def calc(self, regex, chan, nick, **kwargs):
         expr = regex.group('expr')
-        if self.connected:
-            self.protocol.sendDatagram(expr)
+        # transport seems to be nyll if no connection
+        if self.protocol.transport:
+            self.protocol.sendDatagram("sym "+expr)
             self.q.append((chan, expr))
         else:
             self.say(chan, "Not connected to symbolics server")
@@ -57,7 +114,10 @@ class SympyPlugin(Plugin):
 
     def response(self, resp):
         chan, expr = self.q.pop(0)
-        self.say(chan, resp)
+        cmd = resp.split(" ")[0]
+        msg = re.sub(r"\S+ ", "", resp, count=1)
+        print ((cmd, msg))
+        self.say(chan, msg)
 
     def privmsg(self, user, channel, message):
         pass
--- symbolics/management/commands/symbolics_server.py
+++ symbolics/management/commands/symbolics_server.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import
 from django.core.management.base import BaseCommand, CommandError
 import sys
+import re
 
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
@@ -23,19 +24,31 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('subcommand', nargs='?')
-        
+
+        # Named (optional) arguments
+        parser.add_argument(
+            '--port',
+            action='store',
+            help='port to receive packets',
+        )     
 
     def handle(self, *args, **options):
+
+        if options['port']:
+            port = int(options['port'])
+        else:
+            port = 5001
+
         subcommand = options['subcommand']
         if subcommand:
             self.stdout.write('Nothing done')
         else:
-            self.stdout.write('Starting symbolics server')
-            self.start_server()
+            self.stdout.write('Starting symbolics server on port '+str(port))
+            self.start_server(port)
             self.stdout.write('Symbolics server terminated')
 
-    def start_server(self):
-        main()
+    def start_server(self, port):
+        main(port)
 
 class SymVars:
     """ emptry class for storing variables """
@@ -48,30 +61,36 @@ class SymbolicsServerUDP(DatagramProtocol):
         self.nv = MyNodeVisitor()
 
     def datagramReceived(self, datagram, address):
-        print("dg "+ datagram)
-        symvars = SymVars()
-        expr = datagram
-        try:
-            tree = ast.parse(expr)
-            self.nv.initvars(var_prefix = "symvars.")
-            self.nv.visit(tree)
-            print "result = ",self.nv.result
-            print "symbols used : " + str(self.nv.symbol_vars)
-            for v in self.nv.symbol_vars:
-                if not hasattr(symvars, v):
-                    setattr(symvars, v, sympy.symbols(v))
+        cmd = datagram.split(" ")[0]
+        arg = re.sub("\S+ ","",datagram,count=1)
+        print("cmd {} arg {}".format(cmd, arg))
+        if cmd == "hello":
+            self.transport.write("hello", address)
+        elif cmd == "sym":
+            expr = arg
+            symvars = SymVars()
             try:
-                result = eval(self.nv.result)
-                datagram = str(result)
-            except TypeError:
-                datagram = "TypeError"
-        except SyntaxError, e:
-            datagram = "SyntaxError"
-        self.transport.write(datagram, address)
+                tree = ast.parse(expr)
+                self.nv.initvars(var_prefix = "symvars.")
+                self.nv.visit(tree)
+                print "result = ",self.nv.result
+                print "symbols used : " + str(self.nv.symbol_vars)
+                for v in self.nv.symbol_vars:
+                    if not hasattr(symvars, v):
+                        setattr(symvars, v, sympy.symbols(v))
+                try:
+                    result = eval(self.nv.result)
+                    datagram = str(result)
+                except TypeError:
+                    datagram = "TypeError"
+            except SyntaxError, e:
+                datagram = "SyntaxError"
+            datagram = "sym " + datagram
+            self.transport.write(datagram, address)
 
-def main():
-    reactor.listenUDP(5001, SymbolicsServerUDP())
+def main(port):
+    reactor.listenUDP(port, SymbolicsServerUDP())
     reactor.run()
 
 if __name__ == '__main__':
-    main()
+    main(5001)
