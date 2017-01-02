a78a51ca95374225239e47dfa113cfc4a39b5fed
kiwiheretic <kiwiheretic@myself.com>
2017-01-02 14:53:50 -0500

    More urls.py deprecation fixups

--- gcalendar/urls.py
+++ gcalendar/urls.py
@@ -1,20 +1,20 @@
 # urls.py
 from __future__ import absolute_import
 
-from django.conf.urls import patterns, include, url
-from .views import site_setup
+from django.conf.urls import include, url
+import gcalendar.views
 
 # required to set an app name to resolve 'url' in templates with namespacing
 app_name = "gcal"
 
-urlpatterns = patterns('gcalendar.views',
+urlpatterns = [
 #    url(r'^$', 'list'),
-    url(r'^site-setup/$', 'site_setup'),
-    url(r'^user-setup/$', 'user_setup', name='user_setup'),
-    url(r'^callback/$', 'oauth_callback'),
-    url(r'^list/$', 'list', name='list'),
-    url(r'^new-event/$', 'new_event'),
-    url(r'^edit-event/([^/]+)/$', 'edit_event', name='edit-event'),
-    url(r'^event-detail/([^/]+)/$', 'event_detail', name='normal-delete'),
-    url(r'^event-detail/([^/]+)/1/$', 'event_detail', {'recurrence_delete':True}, name='recurrence-delete'),
-)
+    url(r'^site-setup/$', gcalendar.views.site_setup),
+    url(r'^user-setup/$', gcalendar.views.user_setup, name='user_setup'),
+    url(r'^callback/$', gcalendar.views.oauth_callback),
+    url(r'^list/$', gcalendar.views.list, name='list'),
+    url(r'^new-event/$', gcalendar.views.new_event),
+    url(r'^edit-event/([^/]+)/$', gcalendar.views.edit_event, name='edit-event'),
+    url(r'^event-detail/([^/]+)/$', gcalendar.views.event_detail, name='normal-delete'),
+    url(r'^event-detail/([^/]+)/1/$', gcalendar.views.event_detail, {'recurrence_delete':True}, name='recurrence-delete'),
+]
--- reddit/urls.py
+++ reddit/urls.py
@@ -2,28 +2,29 @@
 from __future__ import absolute_import
 
 from django.conf.urls import patterns, include, url
-from .views import site_setup
-from .generic_views import FeedFormView, FeedUpdateFormView, CommentsListView, SubmissionsListView, CommentsDeleteView
+#from .generic_views import FeedFormView, FeedUpdateFormView, CommentsListView, SubmissionsListView, CommentsDeleteView
+import reddit.views
+import reddit.generic_views
 
 app_name = 'reddit'
 
-urlpatterns = patterns('reddit.views',
+urlpatterns = [
 #    url(r'^$', 'list'),
-    url(r'^site-setup/$', 'site_setup', name='site_setup'),
-    url(r'^user-setup/$', 'user_setup', name='user_setup'),
-    url(r'^authorise/$', 'authorise', name='authorise'),
-    url(r'^discard-tokens/$', 'discard_tokens', name='discard_tokens'),
-    url(r'^oauth-callback/$', 'oauth_callback', name='oauth_callback'),
-    url(r'^my-subreddits$', 'my_subreddits', name='mysubreddits'),
-    url(r'^new-post/$', 'new_post', name='new_post'),
-    url(r'^pending-posts/$', SubmissionsListView.as_view(), name='pending_posts'),
-    url(r'^pending-comments/$', CommentsListView.as_view(), name='pending_comments'),
-    url(r'^delete-pending-comments/(?P<pk>\d+)/$', CommentsDeleteView.as_view(), name='delete_pending_comments'),
-    url(r'^feed-add/$', FeedFormView.as_view(), name='feed-add'),
-    url(r'^feed-update/(\d+)/$', FeedUpdateFormView.as_view(), name='feed-update'),
-    url(r'^subreddit-feed-delete/(\d+)/$', 'delete_feed', name='delete_feed'),
-    url(r'^list-subreddit-posts/([^/]+)/$', 'list_posts', name='list_posts'),
-    url(r'^post-detail/(\d+)/$', 'post_detail', name='post_detail'),
-    url(r'^comment-replies/([^/]+)/$', 'comment_replies', name='comment_replies'),
-    url(r'^submit-reply/([^/]+)/$', 'submit_reply', name='submit_reply'),
-)
+    url(r'^site-setup/$', reddit.views.site_setup, name='site_setup'),
+    url(r'^user-setup/$', reddit.views.user_setup, name='user_setup'),
+    url(r'^authorise/$', reddit.views.authorise, name='authorise'),
+    url(r'^discard-tokens/$', reddit.views.discard_tokens, name='discard_tokens'),
+    url(r'^oauth-callback/$', reddit.views.oauth_callback, name='oauth_callback'),
+    url(r'^my-subreddits$', reddit.views.my_subreddits, name='mysubreddits'),
+    url(r'^new-post/$', reddit.views.new_post, name='new_post'),
+    url(r'^pending-posts/$', reddit.generic_views.SubmissionsListView.as_view(), name='pending_posts'),
+    url(r'^pending-comments/$', reddit.generic_views.CommentsListView.as_view(), name='pending_comments'),
+    url(r'^delete-pending-comments/(?P<pk>\d+)/$', reddit.generic_views.CommentsDeleteView.as_view(), name='delete_pending_comments'),
+    url(r'^feed-add/$', reddit.generic_views.FeedFormView.as_view(), name='feed-add'),
+    url(r'^feed-update/(\d+)/$', reddit.generic_views.FeedUpdateFormView.as_view(), name='feed-update'),
+    url(r'^subreddit-feed-delete/(\d+)/$', reddit.views.delete_feed, name='delete_feed'),
+    url(r'^list-subreddit-posts/([^/]+)/$', reddit.views.list_posts, name='list_posts'),
+    url(r'^post-detail/(\d+)/$', reddit.views.post_detail, name='post_detail'),
+    url(r'^comment-replies/([^/]+)/$', reddit.views.comment_replies, name='comment_replies'),
+    url(r'^submit-reply/([^/]+)/$', reddit.views.submit_reply, name='submit_reply'),
+]
